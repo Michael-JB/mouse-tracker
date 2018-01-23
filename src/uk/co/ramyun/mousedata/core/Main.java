9deb62997910ca5de5ef1d142d06f2b9daf1e265
Michael B <michael@ramyun.co.uk>
2018-01-23 10:26:06 +0000

    Initial commit

---
+++ src/uk/co/ramyun/mousedata/core/Main.java
@@ -0,0 +1,19 @@
+package uk.co.ramyun.mousedata.core;
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+@ScriptManifest(author = "Michael", info = "Gathers game input data", name = "Mouse Data", version = 1.00, logo = "")
+public class Main extends Script {
+
+	/**
+	 * @author © Michael 23 Jan 2018
+	 * @file Main.java
+	 */
+
+	@Override
+	public int onLoop() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
